dd05358cfbbcc740a5b32cc0277bae142e910895
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-09-21 17:14:18 +0200

    feat: refactor domain services auth service

--- src/domain/services/index.ts
+++ src/domain/services/index.ts
@@ -1,10 +1,10 @@
-import MailService from '@domain/services/mail';
+import MailService from './mail';
 import AuthService from './auth';
 import TokenService from './token';
 import UserService from './users';
 import TaskService from './task';
 import RedisService from './cache';
-// import MailService from './mail';
+
 
 export {
   AuthService,
